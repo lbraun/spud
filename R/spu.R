2cf726786b6429c77399c02bf31c08fc3fef4a24
Lucas Braun <lucasalan@gmail.com>
2018-08-01 22:06:57 +0200

    Clean up code, get two mapping functions working

--- R/spu.R
+++ R/spu.R
@@ -4,6 +4,7 @@ NULL
 
 library(sf)
 library(mapview)
+library(leaflet)
 
 # spu: R classes and methods for spatial app usage data
 
@@ -19,15 +20,14 @@ library(mapview)
 #'
 #' @examples
 read.spu = function(file, crs = 4326) {
-  data = read.table(system.file("extdata", file, package = "spu"), , header = TRUE, sep = ",")
+  data = read.table(system.file("extdata", file, package = "spu"), header = TRUE, sep = ",")
   data$datetime = strptime(data$datetime, "%Y-%m-%d %H:%M:%S")
   spu_object = st_as_sf(data, coords = c("longitude", "latitude"), crs = crs, agr = "constant")
-  # class(spu_object) = append("spu", class(spu_object))
+  # class(spu_object) = append(class(spu_object), "spu")
   spu_object
 }
 
-
-#' Draw map showing spatial distribution of usage actions
+#' Draw map showing spatial distribution of usage actions using leaflet
 #'
 #' @param data An spu oject containing the data to be displayed
 #'
@@ -35,40 +35,24 @@ read.spu = function(file, crs = 4326) {
 #'
 #' @examples
 plot_usage_actions_leaflet = function(data) {
-  data$color = replicate(nrow(data), sample(c("red", "green", "blue"), 1))
-  data$color = assign_color(data$action)
-  m = leaflet(data) %>% addTiles() %>% addCircles(popup = ~action, color = ~color)
+  # Create a palette that maps factor levels to colors
+  pal = colorFactor(c("navy", "red"), domain = data$action)
+  m = leaflet(data) %>%
+    addTiles() %>%
+    addCircles(popup = ~action, color = ~pal(action)) %>%
+    addLegend(pal = pal, values = ~action, opacity = 1)
   m
 }
 
-plot_usage_actions = function(x) {
-  mapview(x, zcol = c("user", "action"))
-}
-
-assign_colors = function(actions) {
-  for (action in actions)
-  replicate(nrow(data), assign_color(, 1))
-}
-
-assign_color = function(action) {
-  sample(c("red", "green", "blue"), 1)
+#' Draw map showing spatial distribution of usage actions using mapview
+#'
+#' @param data An spu oject containing the data to be displayed
+#'
+#' @export
+#'
+#' @examples
+plot_usage_actions = function(data) {
+  mapview(data, zcol = "action", legend = TRUE)
 }
 
 x = read.spu("dummy_data.csv")
-
-# web_mercator = CRS("+init=epsg:3857")
-
-# library(ggmap)
-# bgMap = get_map(as.vector(bbox(data)), source = "google", zoom = 13)
-# par(mar = rep(0,4))
-# plot(data, bgMap = bgMap, pch = 16, cex = .5)
-
-
-# popup = function(datetime, action, user) {
-#   str_interp("datetime: ${datetime}\naction: ${action}\nuser: ${user}\n")
-# }
-
-# library(leaflet)
-
-# m = leaflet(data) %>% addTiles() %>% addCircles(popup = ~action)
-# m
