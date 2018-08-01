9db44e524ad68982e71352da205e19792aabaa0f
Lucas Braun <lucasalan@gmail.com>
2018-08-01 15:47:24 +0200

    Move in code and data

---
+++ R/spu.R
@@ -0,0 +1,36 @@
+# spu: R classes and methods for spatial app usage data
+
+# install.packages("leaflet")
+
+read.spu = function(file, header = TRUE, sep = ",") {
+  spu_object = read.table(file = file, header = header, sep = sep)
+  spu_object = read.table(system.file("extdata", file, package = "trackz"))
+  spu_object$datetime = strptime(spu_object$datetime, "%Y-%m-%d %H:%M:%S")
+  class(spu_object) = append("spu", class(spu_object))
+  spu_object
+}
+
+plot.spu = function(x, xlab = "Longitude", ylab = "Latitude", pch = "+", ...) {
+  plot(x = x$longitude, y = x$latitude, xlab = xlab, ylab = ylab, pch = pch, ...)
+}
+
+data = read.spu("dummy_data.csv")
+
+plot(data$latitude, data$longitude)
+
+web_mercator = CRS("+init=epsg:3857")
+
+library(ggmap)
+bgMap = get_map(as.vector(bbox(data)), source = "google", zoom = 13)
+par(mar = rep(0,4))
+plot(data, bgMap = bgMap, pch = 16, cex = .5)
+
+
+popup = function(datetime, action, user) {
+  stringr::str_interp("datetime: ${datetime}\naction: ${action}\nuser: ${user}\n")
+}
+
+library(leaflet)
+
+m = leaflet(data) %>% addTiles() %>% addCircles(popup = ~action)
+m
